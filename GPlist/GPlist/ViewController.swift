8c4e2349f78196fbf6ea356638e4f78900bb0c77
geonhwi <g01063962671@gmail.com>
2019-01-12 16:37:30 +0900

    plistEX

---
+++ GPlist/GPlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPlist
+//
+//  Created by 정건휘 on 12/01/2019.
+//  Copyright © 2019 gunw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
